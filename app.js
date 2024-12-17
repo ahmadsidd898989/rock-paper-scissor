7f2a2a765dcaf42c9acd5c2e879413ef0eb7f225
ahmadsidd898989 <ahmadsiddiqui909@gmail.com>
2024-12-17 23:50:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const playgame = (userchoice) => {
         } else if ( userchoice === "paper"){
             userwin = compchoice === "Scissors" ? false : true;
         } else {
-            userwin = compchoice === "paper" ? true : false;
+            userwin = compchoice === "rock" ? false : true;
         }
         showWinner(userwin, userchoice, compchoice);
     } 
